88e3ab5ebb10d2df748590648a95f98ff4a7c5a2
amiramaher <34419964+amiramaher@users.noreply.github.com>
2017-12-20 15:19:39 +0200

    Initial commit

---
+++ eduraka/src/eduraka_test/Day1.java
@@ -0,0 +1,20 @@
+package eduraka_test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.*;
+import org.openqa.selenium.chrome.ChromeDriver;;
+
+public class Day1 {
+	
+	WebDriver driver;
+	public void invokeBrowser(){
+		driver = new ChromeDriver();
+	}
+	
+	public static void main(String[]args){
+					
+		
+		
+		
+	}
+
+}
